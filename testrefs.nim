2853bc4914e5a314e977831c621070bbd6e4efe1
Michael Schwager <nobody@anywhere.com>
2026-02-17 08:18:18 -0800

    Some progress

--- testrefs.nim
+++ testrefs.nim
@@ -1,37 +1,75 @@
-import json, jsony, tables
+import std/[json, math, strutils, tables]
+import jsony
 
 
 
 type
   VarKind = enum vkInt, vkFloat, vkString
-  ShapeKind = enum skRect, skLine
-  JVarVal = object
+  JVal = object
     case kind: VarKind
     of vkInt: intVal: int
     of vkFloat: floatVal: float
     of vkString: strVal: string
+
+  JValRef = object of RootObj
+    case kind: VarKind 
+    of vkInt: intVal: ref int
+    of vkFloat: floatVal: ref float
+    of vkString: stringVal: ref string
+
+  ShapeKind = enum skRect, skLine
   JShape = object
     case kind: ShapeKind
-    of skRect: x,y,w,h: float
-    of skLine: x0,y0,x1,y1: float
+    of skRect: x, y, w, h: JValRef
+    of skLine: x0, y0, x1, y1: JValRef
   JDoc = object
-    vars: Table[string, JVarVal]
+    vars: Table[string, JVal]
     shapes: Table[string, JShape]
 
+proc parseHook(s: string, i: var int, val: var JValRef) =
+  var xf: float
+  var xi: int
+  var xs: string
+  var ii = i
+  echo "i before: ", i
+  echo "ii before: ", ii
+  try:
+    parseHook(s, i, xf)
+    parseHook(s, ii, xi)
+    if xf.round == xf:
+      var x = new int
+      x[] = xi
+      val = JValRef(kind: vkInt, intVal: x)
+    else:
+      var x = new float
+      x[] = xf
+      val = JValRef(kind: vkFloat, floatVal: x)
+  except Exception as e:
+    echo e.msg
+    echo "i during1: ", i
+    parseHook(s, i, xs)
+    echo "i during2: ", i
+    var x = new string
+    x[] = xs
+    val = JValRef(kind: vkString, stringVal: x)
+    
+  echo "i after: ", i
+  echo "ii after: ", ii
+
+  
+
+proc `$`(x: ref SomeNumber):    string = $x[]
+# proc `$`(x: ref float):  string = $x[]
+proc `$`(x: ref string): string = "\"" & x[] & "\""
+
+var jvr = """[ 1, "2", 3.14, 5.0]""".fromJson(seq[JValRef])
+echo jvr
+
+# var jv = readFile("data.json").fromJson(JDoc)
+# echo jv
 
 
-var jv = readFile("data.json").fromJson(JDoc)
-echo jv
 
-
-
-#   JVal = object of RootObj
-#     name: string
-#     case kind: RefKind 
-#       of rkInt: intVal: ref int
-#       of rkFloat: floatVal: ref float
-#       of rkString: stringVal: ref string
-#   VarTable = Table[string, ref JVal]
 
 # proc newRefInt(name: string, val: int): ref JVal =
 #   let iref = new int
@@ -52,8 +90,6 @@ echo jv
 #   case jvalRef.kind
 #   of rkInt: result = result & $jvalRef.intVal
 #   of rkFloat: result = result & $jvalRef.floatVal
-# proc `$`(iRef: ref int): string = $iRef[]
-# proc `$`(fRef: ref float): string = $fRef[]
 
 
 # var varTableRef = new VarTable
