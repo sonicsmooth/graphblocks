21263c0a800f6edaa7ad5653f6412795d2bc9570
sonicsmooth <mn22ywp9nq@snkmail.com>
2026-02-20 12:06:44 -0800

    Added some number tests

--- testrefs.nim
+++ testrefs.nim
@@ -38,6 +38,7 @@ proc parseHook(s: string, i: var int, val: var JVal) =
   try:
     parseHook(s, i, xf)
     parseHook(s, ii, xi)
+    # Need to distinguish between ints and various types of floats
     if xf.round == xf:
       var xref = new int
       xref[] = xi
@@ -89,37 +90,79 @@ proc `$`(x: ref string): string = "→\"" & x[] & "\""
 
 try:
 
-  echo "Doing JPoint"
-  echo """ "(1, 2)"  """.fromJson(JPoint)
-  echo ""
+  echo "Doing JVal"
+  echo """ 42 """.fromJson(JVal)
+  echo """ +42 """.fromJson(JVal)
+  echo """ -42 """.fromJson(JVal)
+  echo """ 42e5 """.fromJson(JVal)
+  echo """ +42e5 """.fromJson(JVal)
+  echo """ -42e5 """.fromJson(JVal)
+  echo """ 42e+5 """.fromJson(JVal)
+  echo """ +42e+5 """.fromJson(JVal)
+  echo """ -42e+5 """.fromJson(JVal)
+  echo """ 42e-5 """.fromJson(JVal)
+  echo """ +42e-5 """.fromJson(JVal)
+  echo """ -42e-5 """.fromJson(JVal)
 
-  echo "Doing seq of JPoints"
-  echo """ ["(1, 2)", "(3, 4)"]  """.fromJson(seq[JPoint])
-  echo ""
+  echo """  19.0 """.fromJson(JVal)
+  echo """ +19.0 """.fromJson(JVal)
+  echo """ -19.0 """.fromJson(JVal)
+  echo """  19.0e5 """.fromJson(JVal)
+  echo """ +19.0e5 """.fromJson(JVal)
+  echo """ -19.0e5 """.fromJson(JVal)
+  echo """  19.0e+5 """.fromJson(JVal)
+  echo """ +19.0e+5 """.fromJson(JVal)
+  echo """ -19.0e+5 """.fromJson(JVal)
+  echo """  19.0e-5 """.fromJson(JVal)
+  echo """ +19.0e-5 """.fromJson(JVal)
+  echo """ -19.0e-5 """.fromJson(JVal)
 
-  echo "Doing JShape with line"
-  echo """{"kind": "skLine", "pt0": "(1, 2)", "pt1": "(3, 4)"}""".fromJson(JShape)
-  echo ""
+  echo """  21.1 """.fromJson(JVal)
+  echo """ +21.1 """.fromJson(JVal)
+  echo """ -21.1 """.fromJson(JVal)
+  echo """  21.1e5 """.fromJson(JVal)
+  echo """ +21.1e5 """.fromJson(JVal)
+  echo """ -21.1e5 """.fromJson(JVal)
+  echo """  21.1e+5 """.fromJson(JVal)
+  echo """ +21.1e+5 """.fromJson(JVal)
+  echo """ -21.1e+5 """.fromJson(JVal)
+  echo """  21.1e-5 """.fromJson(JVal)
+  echo """ +21.1e-5 """.fromJson(JVal)
+  echo """ -21.1e-5 """.fromJson(JVal)
 
-  echo "Doing JShape with polyline"
-  echo """{"kind": "skPolyLine", "pts": ["(1, 2)", "(3, 4)"]}""".fromJson(JShape)
-  echo ""
 
-  echo "Doing JShape with polyline"
-  echo """{"kind": "skPolyLine", "pts": ["(1.5, 2.5)", "($var1, $var2)"]}""".fromJson(JShape)
-  echo ""
 
-  echo "Doing JShape with rect"
-  echo """{"kind": "skRect", "pos": "(12, 34)", "w": 5, "h": 7.5}""".fromJson(JShape)
-  echo ""
+  # echo "Doing JPoint"
+  # echo """ "(1, 2)"  """.fromJson(JPoint)
+  # echo ""
 
-  echo "Doing JShape with rect"
-  echo """{"kind": "skRect", "pos": "(12, 34)", "w": "boxWidth", "h": "boxHeight"}""".fromJson(JShape)
-  echo ""
+  # echo "Doing seq of JPoints"
+  # echo """ ["(1, 2)", "(3, 4)"]  """.fromJson(seq[JPoint])
+  # echo ""
 
-  echo "Doing doc from file"
-  echo readFile("data.json").fromJson(JDoc)
-  echo ""
+  # echo "Doing JShape with line"
+  # echo """{"kind": "skLine", "pt0": "(1, 2)", "pt1": "(3, 4)"}""".fromJson(JShape)
+  # echo ""
+
+  # echo "Doing JShape with polyline"
+  # echo """{"kind": "skPolyLine", "pts": ["(1, 2)", "(3, 4)"]}""".fromJson(JShape)
+  # echo ""
+
+  # echo "Doing JShape with polyline"
+  # echo """{"kind": "skPolyLine", "pts": ["(1.5, 2.5)", "($var1, $var2)"]}""".fromJson(JShape)
+  # echo ""
+
+  # echo "Doing JShape with rect"
+  # echo """{"kind": "skRect", "pos": "(12, 34)", "w": 5, "h": 7.5}""".fromJson(JShape)
+  # echo ""
+
+  # echo "Doing JShape with rect"
+  # echo """{"kind": "skRect", "pos": "(12, 34)", "w": "boxWidth", "h": "boxHeight"}""".fromJson(JShape)
+  # echo ""
+
+  # echo "Doing doc from file"
+  # echo readFile("data.json").fromJson(JDoc)
+  # echo ""
 
 except Exception as e:
   echo e.msg
